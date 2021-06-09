122e2655458f37f82c396758051736a254726ff8
Spidey507 <miguelalveo@gmail.com>
2021-06-09 08:59:35 -0500

    init setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <section className="componentes">
+        <Login/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import logo from '../logo.png';
+import '../App.css';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+class Login extends React.Component{
+    render(){
+        const classes = useStyles;
+        return(
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography component="h1" variant="h5" color="primary">
+            Sign in
+            </Typography>
+            <Container component="main" maxWidth="xs">
+            <form className={classes.form} noValidate>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required= "true"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+            >
+                Sign In
+            </Button>
+            </form>
+            </Container>  
+        </header>
+        );
+    }
+}
+
+export default Login;
